e4ed1c2add1a1516ec3cd7986f489ae0d3eea517
Rob Tuinte <robtuinte15@gmail.com>
2020-06-05 10:41:31 +0200

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,12 +27,13 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-
-  config.assets.compile = true
 
   #test for pics in image
-  config.serve_static_assets = true
+  config.serve_static_files = true
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
