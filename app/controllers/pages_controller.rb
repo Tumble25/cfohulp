a0cdf9deb82ee86feba9cf937d0f3938cc3212e1
Rob Tuinte <robtuinte15@gmail.com>
2020-06-05 10:18:52 +0200

    added everything for heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :bedrijven, :investeerders, :over_CFOhulp]
 
   def home
+  end
+
+  def bedrijven
+  end
+
+   def investeerders
+  end
+
+  def over_CFOhulp
   end
 end
