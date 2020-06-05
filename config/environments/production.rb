55680760c9533df30842a22f5c30196e7026fa2b
Rob Tuinte <robtuinte15@gmail.com>
2020-06-05 10:39:40 +0200

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
+  config.assets.compile = true
+
+  #test for pics in image
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
